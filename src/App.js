dc6e56f2ebd296c89258fed9f301dc69f54dab29
Sellentis <JaredBay1000@gmail.com>
2022-09-18 19:08:52 +0300

    first init task3 extension

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scss/main.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>test react extension</h1>
+    </div>
+  );
+}
+
+export default App;
